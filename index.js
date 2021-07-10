ee73d50f15830d7c8e57c4435b4eeda268a3a259
KeepSOBP <osu@kakao.com>
2021-07-10 20:09:59 +0900

    chore(index): remove djs-light options :<

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const Discord = require("discord.js")
 const client = new Discord.Client({
     disableEveryone: true,
-    intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_VOICE_STATES", "GUILD_MESSAGE_TYPING"],
-    cacheEmojis: true
+    intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_VOICE_STATES", "GUILD_MESSAGE_TYPING"]
 })
 const fs = require("fs")
 const Dokdo = require("dokdo")
