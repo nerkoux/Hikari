0f5937fd3a101b4878861cd3871909fccbc7a45c
KeepSOBP <osu@kakao.com>
2021-07-10 21:00:35 +0900

    refactor(commands): client

--- events/commands.js
+++ events/commands.js
@@ -17,72 +17,73 @@ module.exports = (client, DokdoHandler) => {
         }
     })
 
-    client.on("ready", () => {
-        console.log(`[client] ${client.user.username} ready!`)
-        setInterval(() => {
-            const server = client.guilds.cache.size
-            const cstatuslist = [
-                `${config.prefix}도움`,
-                `${config.prefix}초대`,
-                `${server} 서버`
-            ]
-            const index = Math.floor(Math.random() * cstatuslist.length)
-            client.user.setActivity(cstatuslist[index] + " | 보이스채널", { type: "COMPETING" })
-        }, 10000)
-    })
+    client
+        .on("ready", () => {
+            console.log(`[client] ${client.user.username} ready!`)
+            setInterval(() => {
+                const server = client.guilds.cache.size
+                const cstatuslist = [
+                    `${config.prefix}도움`,
+                    `${config.prefix}초대`,
+                    `${server} 서버`
+                ]
+                const index = Math.floor(Math.random() * cstatuslist.length)
+                client.user.setActivity(cstatuslist[index] + " | 보이스채널", { type: "COMPETING" })
+            }, 10000)
+        })
 
-    // client.on("debug", (e) => console.log(e))
-    client.on("warn", (info) => console.warn(info))
-    client.on("error", (error) => console.error(error))
+    //  .on("debug", (info) => console.log(info))
+        .on("warn", (info) => console.warn(info))
+        .on("error", (error) => console.error(error))
 
-    client.on("messageCreate", async message => {
-        if (!message.guild || message.author.bot || message.channel.type === "dm") return
-        const escapeRegex = (str) => str.replace(/[.*+?^${}()|[\]\\]/gu, "\\$&")
-        const prefixRegex = new RegExp(`^(<@!?${client.user.id}>|${escapeRegex(config.prefix)})\\s*`, "u")
-        if (!prefixRegex.test(message.content)) return
+        .on("messageCreate", async message => {
+            if (!message.guild || message.author.bot || message.channel.type === "dm") return
+            const escapeRegex = (str) => str.replace(/[.*+?^${}()|[\]\\]/gu, "\\$&")
+            const prefixRegex = new RegExp(`^(<@!?${client.user.id}>|${escapeRegex(config.prefix)})\\s*`, "u")
+            if (!prefixRegex.test(message.content)) return
 
-        const [, matchedPrefix] = message.content.match(prefixRegex)
+            const [, matchedPrefix] = message.content.match(prefixRegex)
 
-        const args = message.content.slice(matchedPrefix.length).trim().split(/ +/u)
-        const commandName = args.shift().toLowerCase()
+            const args = message.content.slice(matchedPrefix.length).trim().split(/ +/u)
+            const commandName = args.shift().toLowerCase()
 
-        DokdoHandler.run(message)
+            DokdoHandler.run(message)
 
-        const command =
+            const command =
             client.commands.get(commandName) ||
             client.commands.find((cmd) => cmd.aliases && cmd.aliases.includes(commandName))
 
-        if (!command) return
+            if (!command) return
 
-        if (!client.cooldowns.has(command.name)) {
-            client.cooldowns.set(command.name, new Discord.Collection())
-        }
-
-        const now = Date.now()
-        const timestamps = client.cooldowns.get(command.name)
-        const cooldownAmount = (command.cooldown || 1) * 1000
-
-        if (timestamps.has(message.author.id)) {
-            const expirationTime = timestamps.get(message.author.id) + cooldownAmount
-
-            if (now < expirationTime) {
-                const timeLeft = (expirationTime - now) / 1000
-                return message.reply(
-                    `\`${command.name}\` 명령어는 ${timeLeft.toFixed(1)}초 뒤에 다시 사용하실수 있어요! :pensive:`
-                )
+            if (!client.cooldowns.has(command.name)) {
+                client.cooldowns.set(command.name, new Discord.Collection())
             }
-        }
 
-        timestamps.set(message.author.id, now)
-        setTimeout(() => timestamps.delete(message.author.id), cooldownAmount)
+            const now = Date.now()
+            const timestamps = client.cooldowns.get(command.name)
+            const cooldownAmount = (command.cooldown || 1) * 1000
 
-        try {
-            command.run(client, message, args, config)
-        } catch (error) {
-            console.error(error)
-            message.reply(`에러가 발생했습니다.\n${error}`).catch(console.error)
-        }
-    })
+            if (timestamps.has(message.author.id)) {
+                const expirationTime = timestamps.get(message.author.id) + cooldownAmount
+
+                if (now < expirationTime) {
+                    const timeLeft = (expirationTime - now) / 1000
+                    return message.reply(
+                        `\`${command.name}\` 명령어는 ${timeLeft.toFixed(1)}초 뒤에 다시 사용하실수 있어요! :pensive:`
+                    )
+                }
+            }
+
+            timestamps.set(message.author.id, now)
+            setTimeout(() => timestamps.delete(message.author.id), cooldownAmount)
+
+            try {
+                command.run(client, message, args, config)
+            } catch (error) {
+                console.error(error)
+                message.reply(`에러가 발생했습니다.\n${error}`).catch(console.error)
+            }
+        })
 
     client.login(config.token)
 
