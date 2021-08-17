3972919909e29be1cf30719aeffb071d0f891fb8
KeepSOBP <osu@kakao.com>
2021-08-17 19:37:48 +0900

    chore: use client.config

--- commands/music/play.js
+++ commands/music/play.js
@@ -4,14 +4,14 @@ module.exports = {
     name: "재생",
     aliases: ["틀어", "노래"],
     description: "노래를 틀어줍니다",
-    run: async (client, message, args, config) => {
+    run: async (client, message, args) => {
         if (!message.member.voice.channel) return message.reply("보이스채널에 먼저 들어가셔야 해요.")
         if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return message.reply("저런, 저랑 같은 보이스채널에 계셔야 해요.")
         
         const nostring = new MessageEmbed()
             .setTitle("Hikari :heart:")
             .setColor("cbd0ed")
-            .addField(`${config.prefix}재생 <URL/제목>`, "[수많은 사이트들을 지원해요!](https://ytdl-org.github.io/youtube-dl/supportedsites.html)\n**Spotify도 가능해요!**")
+            .addField(`${client.config.prefix}재생 <URL/제목>`, "[수많은 사이트들을 지원해요!](https://ytdl-org.github.io/youtube-dl/supportedsites.html)\n**Spotify도 가능해요!**")
         const string = args.join(" ")
         if (!string) return message.reply({embeds: [nostring]}, { allowedMentions: { repliedUser: false }})
         message.channel.sendTyping()
--- commands/music/stop.js
+++ commands/music/stop.js
@@ -3,11 +3,11 @@ module.exports = {
     aliases: ["멈춰"],
     description: "노래를 정지합니다",
     cooldown: "5",
-    run: async (client, message, args, config) => {
+    run: async (client, message) => {
         if (!message.member.voice.channel) return message.reply("보이스채널에 먼저 들어가셔야 해요.")
         if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return message.reply("저런, 저랑 같은 보이스채널에 계셔야 해요.")
         const queue = client.distube.getQueue(message)
-        if (!queue) return message.reply(`재생중인 노래가 없어요, \`${config.prefix}재생 <URL/제목>\` 명령어로 재생해 주세요.`)
+        if (!queue) return message.reply(`재생중인 노래가 없어요, \`${client.config.prefix}재생 <URL/제목>\` 명령어로 재생해 주세요.`)
         client.distube.stop(message)
     }
 }
